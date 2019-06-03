58623e137bf0173145241286f0a054286705eaad
Columbia91 <alpha.z91@mail.ru>
2019-06-03 21:40:21 +0600

    Добавьте файлы проекта.

---
+++ DomainLessonEvening/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLessonEvening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            AnalizeDomain(currentDomain);
+
+            var secondDomain = AppDomain.CreateDomain("Another Specific Domain Area");
+            AnalizeDomain(secondDomain);
+
+            secondDomain.ExecuteAssembly(secondDomain.BaseDirectory + "KakHotite.exe", new string[] { "5", "7"});
+            AnalizeDomain(secondDomain);
+            
+            AppDomain.Unload(secondDomain);
+
+            Console.ReadLine();
+        }
+
+        private static void AnalizeDomain(AppDomain currentDomain)
+        {
+            Console.WriteLine($"***{currentDomain.FriendlyName}, {currentDomain.BaseDirectory}, {currentDomain.Id}");
+
+            var assemblies = currentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine($"{assembly.FullName}, {assembly.Location}");
+            }
+        }
+    }
+}
